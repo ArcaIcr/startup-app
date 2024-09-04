1b01a910f8a75bf8397be6210f9e004c8f323f6f
Morrix Ken Ohata <morrix.ken@gmail.com>
2024-09-04 16:36:10 +0800

    commit 002

---
+++ src/components/Hero.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  title: {
+    type: String,
+    default: "Become a Vue Dev",
+  },
+  subtitle: {
+    type: String,
+    default: "Find the Vue job fits your skills and need",
+  },
+});
+</script>
+
+<template>
+  <section class="bg-white-700 py-20 mb-4">
+    <div
+      class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+    >
+      <div class="text-side-left">
+        <h1 class="text-4xl font-extrabold text-blue sm:text-5xl md:text-6xl">
+          StartUp Compass
+        </h1>
+        <p class="my-4 text-xl text-blue">
+          Empowering Cagayan de Oro businesses with technology! Our platform
+          provides a comprehensive suite of tools to help local entrepreneurs
+          navigate the challenges of running a business in today's digital
+          world. From inventory management and market analysis to financial
+          planning and trend tracking, we offer the resources you need to
+          thrive.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,52 @@
+<script setup>
+import logo from "@/assets/img/logo.png";
+</script>
+
+<template>
+  <nav class="bg-purple-700 border-b border-white-500">
+    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div class="flex h-20 items-center justify-between">
+        <div
+          class="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
+        >
+          <!-- Logo -->
+          <a class="flex flex-shrink-0 items-center mr-4" href="index.html">
+            <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+            <span class="hidden md:block text-white text-2xl font-bold ml-2"
+              >StartUp Compass</span
+            >
+          </a>
+          <div class="md:ml-auto">
+            <div class="flex space-x-2">
+              <a
+                href="index.html"
+                class="text-white bg-purple-700 hover:bg-violet-900 hover:text-white rounded-md px-3 py-2"
+                >Home</a
+              >
+              <a
+                href="about.html"
+                class="text-white hover:bg-violet-900 hover:text-white rounded-md px-3 py-2"
+                >About</a
+              >
+              <a
+                href="pricing.html"
+                class="text-white hover:bg-violet-900 hover:text-white rounded-md px-3 py-2"
+                >Pricing</a
+              >
+              <a
+                href="log-in.html"
+                class="text-white hover:bg-violet-900 hover:text-white rounded-md px-3 py-2"
+                >Log-in</a
+              >
+              <a
+                href="sign-up.html"
+                class="text-white hover:bg-violet-900 hover:text-white rounded-md px-3 py-2"
+                >Sign-up</a
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
